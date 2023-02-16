5fe5c9f24455d860f16461938b607454dbf6d249
Youssouf OUATTARA <121155997+yous51991@users.noreply.github.com>
2023-02-16 16:58:01 +0000

    Add files via upload

---
+++ exercice.sql
@@ -0,0 +1,26 @@
+CREATE TABLE FirstTab (id integer, name VARCHAR(10))
+INSERT INTO FirstTab
+VALUES (5, 'Pawan'),
+    (6, 'Sharlee'),
+    (7, 'Krish'),
+    (NULL, 'Avtaar');
+SELECT *
+FROM FirstTab;
+CREATE TABLE SecondTab (id integer);
+INSERT INTO SecondTab
+VALUES (5),
+    (NULL);
+SELECT *
+FROM SecondTab;
+
+-- Q1. What will be the OUTPUT of the following statement?
+ the result of this request returns 0
+
+-- Q2. What will be the OUTPUT of the following statement?
+the result of this request returns 2 
+
+-- Q3. What will be the OUTPUT of the following statement?
+the result of this request returns 0
+
+-- Q4. What will be the OUTPUT of the following statement?
+the result of this request returns 2
